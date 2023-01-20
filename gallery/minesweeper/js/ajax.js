3427c255a042c539131b727810ba51f7975ac4fe
Sunrise <yane@88.com>
2023-01-21 01:33:35 +0800

    Tidy up part of codes

--- gallery/minesweeper/js/ajax.js
+++ gallery/minesweeper/js/ajax.js
@@ -1,89 +1,89 @@
-
-
-function $(id){return document.getElementById(id);}
-
-function Init(){
-CreateAjax();
-//document.onmousemove=getMousePosition;
-Start();
+function $(id)
+{
+    return document.getElementById(id);
 }
 
+function Init()
+{
+    CreateAjax();
+    //document.onmousemove=getMousePosition;
+    Start();
+}
 
 var xmlhttp;
 
-function CreateAjax(){
-
-    xmlhttp=null;
-    if (window.XMLHttpRequest){
-      xmlhttp=new XMLHttpRequest(); 
-    }else if(window.ActiveXObject){
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-}
-	
-}
-
-
-function LoadPage(Url,Process)
+function CreateAjax()
 {
-	_datahander=Process;
-	xmlhttp.open("GET", Url,true);
-	xmlhttp.onreadystatechange = Recieve;
-	xmlhttp.send(null);
 
+    xmlhttp = null;
+    if (window.XMLHttpRequest)
+    {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject)
+    {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
 
 }
 
+function LoadPage(Url, Process)
+{
+    _datahander = Process;
+    xmlhttp.open("GET", Url, true);
+    xmlhttp.onreadystatechange = Recieve;
+    xmlhttp.send(null);
+
+}
 
 var _datahander;
 
-function Recieve() 
+function Recieve()
 {
-if (xmlhttp.readyState == 4) {
-	if (xmlhttp.status == 200) {
-		_datahander();
-	} 
-	else{
-		//alert("获取数据异常：" + xmlhttp.status);
-	}
+    if (xmlhttp.readyState == 4)
+    {
+        if (xmlhttp.status == 200)
+        {
+            _datahander();
+        }
+        else
+        {
+            //alert("获取数据异常：" + xmlhttp.status);
+        }
+    }
 }
-}
 
-
-
-var _arr=new Array();
+var _arr = new Array();
 var _rows;
 
 function Parse(SData)
 {
 
+    _arr.length = 0;
 
+    var bulks = SData.split("\x1e");
 
-_arr.length=0;
+    for (var i = 0; i < bulks.length - 1; i++)
+    {
 
-var bulks=SData.split("\x1e");
+        _arr[i] = bulks[i].split("\x1f");
 
+        //_arr[i]=new Array();
 
-for(var i=0;i<bulks.length-1;i++){
+        //var lines=bulks[i].split("\x1e");
 
-	_arr[i]=bulks[i].split("\x1f");
+        //for(var j=0;j<lines.length-1;j++){
 
-	//_arr[i]=new Array();
+        //	var segs=lines[j].split("\x1f");
 
-	//var lines=bulks[i].split("\x1e");
+        //	_arr[i][segs[0]]=segs[1];
 
-	//for(var j=0;j<lines.length-1;j++){
-		
-	//	var segs=lines[j].split("\x1f");
+        //}
 
-	//	_arr[i][segs[0]]=segs[1];
+        //_arr[i]["id"]=lines[j];
 
-	//}
+    }
 
-	//_arr[i]["id"]=lines[j];
-
-}
-
-_rows=bulks[i];
+    _rows = bulks[i];
 
 }
-
--- gallery/minesweeper/js/show2.js
+++ gallery/minesweeper/js/show2.js
@@ -1,104 +1,88 @@
-
-
-function $(id){return document.getElementById(id);}
+function $(id)
+{
+    return document.getElementById(id);
+}
 
 //document.onmousemove=getMousePosition;
 
-function LoadPage(url,func) 
+function LoadPage(url, func)
 {
-var xhr= new XMLHttpRequest();
-xhr.open("GET", url,true); 
-xhr.onreadystatechange = function() { 
-   if(xhr.readyState ==4 && (xhr.status==200||xhr.status==304)){ 
-        func(xhr.responseText); 
-   }
-};
-xhr.send(null);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function ()
+    {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304))
+        {
+            func(xhr.responseText);
+        }
+    };
+    xhr.send(null);
 }
 
-
-var _arr=new Array();
+var _arr = new Array();
 var _rows;
 
-
-var _pgno; 
+var _pgno;
 
 function List(pn)
 {
-_pgno=pn;
+    _pgno = pn;
 
-LoadPage("data.php?m=10&n="+pn,Show);
+    LoadPage("data.php?m=10&n=" + pn, Show);
 }
-
-
-
 
 function Show(SData)
 {
 
+    _arr.length = 0;
 
-_arr.length=0;
+    var bulks = SData.split("\x1e");
 
-var bulks=SData.split("\x1e");
+    for (var i = 0; i < bulks.length - 1; i++)
+    {
 
+        _arr[i] = bulks[i].split("\x1f");
 
-for(var i=0;i<bulks.length-1;i++){
+    }
 
-	_arr[i]=bulks[i].split("\x1f");
+    _rows = bulks[i];
 
+    for (i = 0; i < _arr.length; i++)
+    {
 
-}
+        var person = document.createElement("div")
 
-_rows=bulks[i];
+            person.innerHTML = "<b>" + _arr[i][1] + "</b>于" + new Date(_arr[i][3] * 1000).toLocaleString() + "说：";
+        $("list").appendChild(person);
 
+        var main = document.createElement("div")
+            main.className = "main";
 
+        var says = document.createElement("span");
+        says.innerText = _arr[i][2];
+        says.className = "says";
+        main.appendChild(says);
 
+        var sid = document.createElement("span");
+        sid.innerText = _arr[i][0];
+        sid.className = "sid";
+        main.appendChild(sid);
 
-for(i=0;i<_arr.length;i++){
+        $("list").appendChild(main);
 
-		var person=document.createElement("div")
+    }
 
-		person.innerHTML="<b>"+_arr[i][1]+"</b>于"+new Date(_arr[i][3]*1000).toLocaleString()+"说：";
-		$("list").appendChild(person);
+    var pages = Math.ceil(_rows / 10);
 
-		var main=document.createElement("div")
-		main.className="main";
-
-		var says=document.createElement("span");
-		says.innerText=_arr[i][2];
-		says.className="says";
-		main.appendChild(says);
-
-		var sid=document.createElement("span");
-		sid.innerText=_arr[i][0];
-		sid.className="sid";
-		main.appendChild(sid);
-
-
-		$("list").appendChild(main);
-
-
-}	
-
-
-var pages=Math.ceil(_rows/10);
-
-
-if(_pgno<pages){
-	$("page").innerHTML="<a href=javascript:List("+(_pgno+1)+")>更多...</a>";
-}
-else{
-	$("page").innerHTML="";
-}
-
-
-
+    if (_pgno < pages)
+    {
+        $("page").innerHTML = "<a href=javascript:List(" + (_pgno + 1) + ")>更多...</a>";
+    }
+    else
+    {
+        $("page").innerHTML = "";
+    }
 
 }
-
 
 List(1);
-
-
-
-
